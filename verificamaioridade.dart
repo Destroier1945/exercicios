2e4deb567b5f6d8575e92bbdb2de39f4f5194376
Juliano <kluge233@hotmail.com>
2024-06-12 18:54:26 -0300

    finalizado verifica maioridade

---
+++ verificamaioridade.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+  int sexo;
+  int idade;
+
+  print('Qual o seu sexo? (1)Masc -  (2)Fem');
+  sexo = int.parse(stdin.readLineSync()!);
+  if (sexo == 1) {
+    print('Sexo masculino ');
+  } else if (sexo == 2) {
+    print('Sexo feminino');
+  } else {
+    print('Estrovenga');
+  }
+  print('Qual a sua idade? ');
+  idade = int.parse(stdin.readLineSync()!);
+
+  if (idade < 18) {
+    print('Menor de idade');
+  } else {
+    print('Maior de idade');
+  }
+}
